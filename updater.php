579396dec408203ab0d9b81334641e7f855742de
Matthew Ray <rayman813@yahoo.com>
2015-07-17 13:15:51 -0400

    Updating URI building to use WordPress function add_query_arg instead of sprintf.

--- updater.php
+++ updater.php
@@ -46,23 +46,25 @@ class Smashing_Updater {
 	}
 
 	private function get_repository_info() {
-		if ( is_null( $this->github_response ) ) { // Do we have a response?
-			$request_uri = sprintf( 'https://api.github.com/repos/%s/%s/releases', $this->username, $this->repository ); // Build URI
-			if( $this->authorize_token ) { // Is there an access token?
-				$request_uri = sprintf( '%s?access_token=%s', $request_uri, $this->authorize_token ); // Append it
-			}
-
-			$response = json_decode( wp_remote_retrieve_body( wp_remote_get( $request_uri ) ), true ); // Get JSON and parse it
-			if( is_array( $response) ) { // If it is an array
-				$response = current( $response ); // Get the first item
-			}
-
-			if( $this->authorize_token ) { // Is there an access token?
-				 $response['zipball_url'] = sprintf( '%s?access_token=%s', $response['zipball_url'], $this->authorize_token ); // Update our zip url with token
-			}
-
-			$this->github_response = $response; // Set it to our property			 
-		}
+	    if ( is_null( $this->github_response ) ) { // Do we have a response?
+	        $request_uri = sprintf( 'https://api.github.com/repos/%s/%s/releases', $this->username, $this->repository ); // Build URI
+	        
+	        if( $this->authorize_token ) { // Is there an access token?
+	            $request_uri = add_query_arg( 'access_token', $this->authorize_token, $request_uri ); // Append it
+	        }        
+	        
+	        $response = json_decode( wp_remote_retrieve_body( wp_remote_get( $request_uri ) ), true ); // Get JSON and parse it
+	        
+	        if( is_array( $response ) ) { // If it is an array
+	            $response = current( $response ); // Get the first item
+	        }
+	        
+	        if( $this->authorize_token ) { // Is there an access token?
+	            $response['zipball_url'] = add_query_arg( 'access_token', $this->authorize_token, $response['zipball_url'] ); // Update our zip url with token
+	        }
+	        
+	        $this->github_response = $response; // Set it to our property  
+	    }
 	}
 
 	public function initialize() {
