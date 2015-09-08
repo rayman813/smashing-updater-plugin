32181ff94ca8c97c51ff5a92e2282a03fd2166a0
Matthew Ray <rayman813@yahoo.com>
2015-09-08 13:02:47 -0400

    Fixing duplicate definition
    Checking of multiple plugins are using the same class name for the updater script. This shouldn't be the case, but checking anyway!

--- smashing-plugin.php
+++ smashing-plugin.php
@@ -6,8 +6,9 @@
 	Author: Matthew Ray
 	Author URI: http://www.matthewray.com
 */
-
-include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
+if( ! class_exists( 'Smashing_Updater' ) ){
+	include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
+}
 
 $updater = new Smashing_Updater( __FILE__ );
 $updater->set_username( 'rayman813' );
